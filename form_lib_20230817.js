fd1f2ef5a2870e3f4a5fd8d29e74c7d2fdf370cb
k-sugi <k-sugi@holon.co.jp>
2023-11-29 15:58:07 +0900

    top と left を css に設定

--- form_lib_20230817.js
+++ form_lib_20230817.js
@@ -143,6 +143,8 @@ class IconObjects {
       iconDiv.css('text-align', 'center');
       iconDiv.css('width', `${(this.list[key].string.length + 2) * fontSize}pt`);
       iconDiv.css('height', `${fontSize * 2}pt`);
+      iconDiv.css('top', this.list[key].top);
+      iconDiv.css('left', this.list[key].left);
       console.log(key, this.list[key].pages);
       this.list[key].pages.forEach(page => {
         page.children('[class~="iftc_cf_inputitems"]').append(iconDiv);
