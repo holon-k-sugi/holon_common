83ef502b33fb5fe9fe353c5808bfa4854afdd4ac
k-sugi <k-sugi@holon.co.jp>
2023-11-30 15:41:51 +0900

    absolute の前後にクオートの入れ忘れを修正

--- form_lib_20230817.js
+++ form_lib_20230817.js
@@ -149,7 +149,7 @@ class IconObjects {
       iconDiv.css('height', `${fontSize * 2}pt`);
       iconDiv.css('top', this.list[key].top);
       iconDiv.css('left', this.list[key].left);
-      iconDiv.css('position', absolute);
+      iconDiv.css('position', 'absolute');
       iconDiv.attr('id', this.list[key].name);
       this.list[key].pages.forEach(page => {
         page.children('[class~="iftc_cf_inputitems"]').append(iconDiv);
