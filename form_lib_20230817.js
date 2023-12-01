6535716bdd6a9b7bb653f0a16d9beaa820784af2
k-sugi <k-sugi@holon.co.jp>
2023-12-01 10:27:49 +0900

    class ではなく id を取得するように変更

--- form_lib_20230817.js
+++ form_lib_20230817.js
@@ -83,11 +83,8 @@ class PageList {
   getInitialPageCount() {
     const tmp = new Set();
     const ret = Object.values(this.list).findIndex(v => {
-      console.log([...v.classList.values()]);
-      const str = [...v.classList.values()].find(v => { v.indexOf('iftc_cf_page_') > -1 });
-      console.log(str);
-      if (tmp.has(str)) return true;
-      tmp.add(str);
+      if (tmp.has(v.id)) return true;
+      tmp.add(v.id);
       return false;
     });
     return ret === -1 ? Object.values(this.list).length : ret;
