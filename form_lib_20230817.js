d79ca09374fbb0e4589f8225850af0385cc93794
k-sugi <k-sugi@holon.co.jp>
2023-12-04 14:27:54 +0900

    log を追加

--- form_lib_20230817.js
+++ form_lib_20230817.js
@@ -172,6 +172,8 @@ class IconObjects {
 
     Object.keys(this.list[name].margin).forEach((key, i) => {
       const id = $(`#${page.attr('id')} [id~="iftc_cf_inputarea"]`).attr('id');
+      console.log($(`#${page.attr('id')}`));
+      console.log($(`#${page.attr('id')} [id~="iftc_cf_inputarea"]`));
       console.log(id);
       console.log(getPtValueFromStylesheets(`#${id}`));
       // console.log(getPtValueFromStylesheets(`#${page.attr('id')} [class~="iftc_cf_inputitems"]`)[key]);
