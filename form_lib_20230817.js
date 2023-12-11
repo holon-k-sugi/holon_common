1086e06fd526d1e4b878d3e9213207f5eb225031
k-sugi <k-sugi@holon.co.jp>
2023-12-11 13:36:50 +0900

    log の追加

--- form_lib_20230817.js
+++ form_lib_20230817.js
@@ -870,6 +870,7 @@ function createCSVLabel() {
     'align-items': 'center'
   };
   // CSV項目の中で hidden に設定されているオブジェクトを udefined に設定する。
+  console.log(allCsvObj);
   const visibleObj = allCsvObj.map((csv, i) => {
     if (!inputObjects.objExists(csv)) {
       console.warn(`CSV番号 ${i + 1} 番: ${csv} は存在しないオブジェクト`);
