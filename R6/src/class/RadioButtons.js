643ad5d193117dd5b62aad49eecf28573c9b92de
k-sugi <k-sugi@holon.co.jp>
2024-12-19 09:26:02 +0900

    ラジオボタンのグループ名の取得方法の誤りを修正

--- R6/src/class/RadioButtons.js
+++ R6/src/class/RadioButtons.js
@@ -19,7 +19,7 @@ class RadioButtons {
   }
 
   static onClickRadioButtonL(name, index) {
-    const groupName = name.split('_').slice(1).join('_');
+    const groupName = name.split('_').slice(0, -1).join('_');
     const preState = getV(...[name, index].filter(v => v !== undefined));
     RadioButtons.getRadioGroup(groupName).getAllButtonNameList().forEach(buttonName => {
       const tmp = [buttonName, index, RadioButtons.getRadioGroup(groupName).unmark].filter(v => v !== undefined);
