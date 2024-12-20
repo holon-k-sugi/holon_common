cff4e68e476eb6481e067575eda353f85b31f5b5
k-sugi <k-sugi@holon.co.jp>
2024-12-20 09:53:27 +0900

    ログの内容を変更

--- R6/dist/form_lib_20240912.js
+++ R6/dist/form_lib_20240912.js
@@ -741,7 +741,7 @@ class RadioButtons {
   }
 
   static onClickRadioButtonL(name, index) {
-    console.log('fuga');
+    console.log(name, index);
     const groupName = name.split('_').slice(0, -1).join('_');
     const preState = getV(...[name, index].filter(v => v !== undefined));
     RadioButtons.getRadioGroup(groupName).getAllButtonNameList().forEach(buttonName => {
