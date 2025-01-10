7c064d52fa32f687aa008fdc00081ca22c6b4fac
k-sugi <k-sugi@holon.co.jp>
2025-01-10 11:38:49 +0900

    InputObjectsByName の定義位置を InputObjects より前に変更

--- R6/dist/form_lib_20240912.js
+++ R6/dist/form_lib_20240912.js
@@ -1,3 +1,65 @@
+class InputObjectsByName {
+  constructor(maxPageNum) {
+    this.objList = [];
+    this.objListByPage = [...Array(maxPageNum)].map(() => []);
+    this.type = '';
+  }
+
+  register(id, page) {
+    this.objList.push(id);
+    this.objListByPage[page].push(id);
+    if (this.type === '') this.type = $(`#${id}`).prop('type');
+  }
+
+  getId() {
+    return this.objList[0];
+  }
+
+  getALLIds() {
+    return this.objList;
+  }
+
+  getPageList() {
+    return this.objListByPage.map((n, i) => {
+      if (n.length > 0) return i + 1;
+    }).filter(v => v !== undefined);
+  }
+
+  getIdsByPage(page) {
+    return this.objListByPage[page];
+  }
+
+  getFilteredList() {
+    return this.objListByPage.filter(v => v.length !== 0);
+  }
+
+  getIdsByIndex(index) {
+    return this.getFilteredList()[index];
+  }
+
+  getType() {
+    return this.type;
+  }
+
+  getValueByIndex(index) {
+    const id = this.getIdsByIndex(index)[0];
+    if (this.type === 'checkbox') return $(`#${id}`).prop('checked');
+    if (this.type === 'text') return $(`#${id}`).val();
+    return '';
+  }
+
+  getValue() {
+    return this.getValueByIndex(0);
+  }
+
+  getIndexById(id) {
+    return this.getFilteredList().findIndex(arr => arr.some(v => v === id));
+  }
+
+  getLengthOfPage() {
+    return this.getFilteredList().length;
+  }
+}
 class InputObjects {
   static #MAX_PAGE_NUM = 50;
   static #list = {};
@@ -536,68 +598,6 @@ class IconObjects {
       return false;
     }));
     return result;
-  }
-}
-class InputObjectsByName {
-  constructor(maxPageNum) {
-    this.objList = [];
-    this.objListByPage = [...Array(maxPageNum)].map(() => []);
-    this.type = '';
-  }
-
-  register(id, page) {
-    this.objList.push(id);
-    this.objListByPage[page].push(id);
-    if (this.type === '') this.type = $(`#${id}`).prop('type');
-  }
-
-  getId() {
-    return this.objList[0];
-  }
-
-  getALLIds() {
-    return this.objList;
-  }
-
-  getPageList() {
-    return this.objListByPage.map((n, i) => {
-      if (n.length > 0) return i + 1;
-    }).filter(v => v !== undefined);
-  }
-
-  getIdsByPage(page) {
-    return this.objListByPage[page];
-  }
-
-  getFilteredList() {
-    return this.objListByPage.filter(v => v.length !== 0);
-  }
-
-  getIdsByIndex(index) {
-    return this.getFilteredList()[index];
-  }
-
-  getType() {
-    return this.type;
-  }
-
-  getValueByIndex(index) {
-    const id = this.getIdsByIndex(index)[0];
-    if (this.type === 'checkbox') return $(`#${id}`).prop('checked');
-    if (this.type === 'text') return $(`#${id}`).val();
-    return '';
-  }
-
-  getValue() {
-    return this.getValueByIndex(0);
-  }
-
-  getIndexById(id) {
-    return this.getFilteredList().findIndex(arr => arr.some(v => v === id));
-  }
-
-  getLengthOfPage() {
-    return this.getFilteredList().length;
   }
 }
 class LazyEvaluationFunctions {
