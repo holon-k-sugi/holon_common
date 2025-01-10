55117f5a9442aa02ab36dc9cae25a4dd074e0e18
k-sugi <k-sugi@holon.co.jp>
2025-01-10 11:31:22 +0900

    InputObjects を先頭に定義するように修正

--- R6/dist/form_lib_20240912.js
+++ R6/dist/form_lib_20240912.js
@@ -1,3 +1,100 @@
+class InputObjects {
+  static #MAX_PAGE_NUM = 50;
+  static #list = {};
+  static #objListByPage = [];
+
+  static initialize() {
+    this.#objListByPage = Array.from({ length: this.#MAX_PAGE_NUM }, () => []);
+    this.#list = allObj.reduce(this.#processObject.bind(this), {});
+  }
+
+  static #processObject(acc, id) {
+    const { objName, page } = this.#parseId(id);
+    this.#objListByPage[page].push({ name: objName, id });
+
+    if (!acc[objName]) acc[objName] = new InputObjectsByName(this.#MAX_PAGE_NUM);
+    acc[objName].register(id, page);
+
+    return acc;
+  }
+
+  static #parseId(id) {
+    const splitId = id.split('_');
+    const slicedId = splitId.slice(1, -1);
+    const page = parseInt(slicedId.pop(), 10);
+    const objName = slicedId.join('_');
+    return { objName, page };
+  }
+
+  static getAllObjNameList() {
+    return Object.keys(this.#list);
+  }
+
+  static getObjByName(name) {
+    if (this.#list[name] === undefined) {
+      logWarningWithCaller(`${name} は存在しないオブジェクト`);
+      return new InputObjectsByName(this.#MAX_PAGE_NUM);
+    }
+    return this.#list[name];
+  }
+
+  static getAllIds(name) {
+    return InputObjects.getObjByName(name).getALLIds();
+  }
+
+  static getLengthOfPageListByName(name) {
+    return InputObjects.getObjByName(name).getLengthOfPage();
+  }
+
+  static getIdsbyPage(name, page) {
+    if (InputObjects.getObjByName(name).getIdsByPage(page) === undefined) {
+      console.warn(`${page} ページ目に ${name} は存在しない`);
+      return [];
+    }
+    return InputObjects.getObjByName(name).getIdsByPage(page);
+  }
+
+  static getIdsByIndex(name, index) {
+    const list = InputObjects.getObjByName(name).getFilteredList();
+    if (list[index] === undefined) {
+      console.warn(`${name} が存在するページ数は ${index + 1} より少ない`);
+      return [];
+    }
+    return list[index];
+  }
+
+  static objExists(name) {
+    return this.#list[name] !== undefined;
+  }
+
+  static getIndexById(id) {
+    const splitId = id.split('_');
+    splitId.shift(); splitId.pop(); splitId.pop();
+    const objName = splitId.join('_');
+    return this.#list[objName].getIndexById(id);
+  }
+
+  static getObjListByPage(page) {
+    return this.#objListByPage[page];
+  }
+
+  static getValue(name) {
+    return InputObjects.getObjByName(name).getValue();
+  }
+
+  static getValueByIndex(name, index) {
+    return InputObjects.getObjByName(name).getValueByIndex(index ?? 0);
+  }
+
+  static setValueByIndex(...args) {
+    const target = args.length === 2 || (args.length === 3 && args[1] === undefined)
+      ? InputObjects.getAllIds(args[0]) : InputObjects.getIdsByIndex(args[0], args[1]);
+    const val = args.slice(-1)[0];
+    target.forEach(id => {
+      $(`#${id}`).val(val);
+    });
+  }
+}
 class ChechBox {
   static #list = {};
   static initialize() {
@@ -439,103 +536,6 @@ class IconObjects {
       return false;
     }));
     return result;
-  }
-}
-class InputObjects {
-  static #MAX_PAGE_NUM = 50;
-  static #list = {};
-  static #objListByPage = [];
-
-  static initialize() {
-    this.#objListByPage = Array.from({ length: this.#MAX_PAGE_NUM }, () => []);
-    this.#list = allObj.reduce(this.#processObject.bind(this), {});
-  }
-
-  static #processObject(acc, id) {
-    const { objName, page } = this.#parseId(id);
-    this.#objListByPage[page].push({ name: objName, id });
-
-    if (!acc[objName]) acc[objName] = new InputObjectsByName(this.#MAX_PAGE_NUM);
-    acc[objName].register(id, page);
-
-    return acc;
-  }
-
-  static #parseId(id) {
-    const splitId = id.split('_');
-    const slicedId = splitId.slice(1, -1);
-    const page = parseInt(slicedId.pop(), 10);
-    const objName = slicedId.join('_');
-    return { objName, page };
-  }
-
-  static getAllObjNameList() {
-    return Object.keys(this.#list);
-  }
-
-  static getObjByName(name) {
-    if (this.#list[name] === undefined) {
-      logWarningWithCaller(`${name} は存在しないオブジェクト`);
-      return new InputObjectsByName(this.#MAX_PAGE_NUM);
-    }
-    return this.#list[name];
-  }
-
-  static getAllIds(name) {
-    return InputObjects.getObjByName(name).getALLIds();
-  }
-
-  static getLengthOfPageListByName(name) {
-    return InputObjects.getObjByName(name).getLengthOfPage();
-  }
-
-  static getIdsbyPage(name, page) {
-    if (InputObjects.getObjByName(name).getIdsByPage(page) === undefined) {
-      console.warn(`${page} ページ目に ${name} は存在しない`);
-      return [];
-    }
-    return InputObjects.getObjByName(name).getIdsByPage(page);
-  }
-
-  static getIdsByIndex(name, index) {
-    const list = InputObjects.getObjByName(name).getFilteredList();
-    if (list[index] === undefined) {
-      console.warn(`${name} が存在するページ数は ${index + 1} より少ない`);
-      return [];
-    }
-    return list[index];
-  }
-
-  static objExists(name) {
-    return this.#list[name] !== undefined;
-  }
-
-  static getIndexById(id) {
-    const splitId = id.split('_');
-    splitId.shift(); splitId.pop(); splitId.pop();
-    const objName = splitId.join('_');
-    return this.#list[objName].getIndexById(id);
-  }
-
-  static getObjListByPage(page) {
-    return this.#objListByPage[page];
-  }
-
-  static getValue(name) {
-    return InputObjects.getObjByName(name).getValue();
-  }
-
-  static getValueByIndex(name, index) {
-    return InputObjects.getObjByName(name).getValueByIndex(index ?? 0);
-  }
-
-  static setValueByIndex(...args) {
-    const target = args.length === 2 || (args.length === 3 && args[1] === undefined)
-      ? InputObjects.getAllIds(args[0]) : InputObjects.getIdsByIndex(args[0], args[1]);
-    const val = args.slice(-1)[0];
-    target.forEach(id => {
-      $(`#${id}`).val(val);
-    });
   }
 }
 class InputObjectsByName {
