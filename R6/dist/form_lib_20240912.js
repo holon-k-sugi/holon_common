4f2dbf90e2008815337f7efd1fa75cdfda7753b2
k-sugi <k-sugi@holon.co.jp>
2025-01-24 13:47:53 +0900

    log の追加

--- R6/dist/form_lib_20240912.js
+++ R6/dist/form_lib_20240912.js
@@ -307,6 +307,7 @@ class EmployeesContents {
         }
       });
     });
+    console.log(this.#list);
     // 書類の内容を上書き
     Object.keys(employees.list).forEach(key => {
       [...Array(employees.max)].forEach((_, i) => {
